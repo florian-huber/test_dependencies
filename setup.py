adb95a57285e020f78aee03458068acd01f445e6
Florian Huber <36473328+florian-huber@users.noreply.github.com>
2021-11-01 13:02:55 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='test_dependencies',
+      version='0.1.0',
+      description='just playing around',
+      long_description=readme(),
+      classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+      ],
+      packages=['memo_ms'],
+      python_requires=">=3.8",
+      install_requires=[
+          'pandas',
+          'numpy',
+          'matchms >= 0.9.0',
+          'spec2vec >= 0.4',
+          'plotly >= 4.14.3',
+          'cimcb-lite >= 1.0.2',
+          'scikit-bio >= 0.5.6',
+          'scikit-learn >= 0.24.1'
+      ],
+      zip_safe=False)
